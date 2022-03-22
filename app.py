1ff45a0397548691303ff3aa12b920837089f751
Chris Rodea <Chrisrodea@gmail.com>
2022-03-21 22:23:54 -0700

    Created a TODO listfor how I want the bot to respond to comments when 'called'

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import praw
 #! Login credentials are in the "praw.ini" file
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("Eldenring")
+subreddit = reddit.subreddit("Eldenring") #! Will change the subreddit later once bot is complete
 ##subreddit = reddit.subreddit("AedorBotTest")
 
 #! Prints the name of the account that is logged in
@@ -14,42 +14,47 @@ for comment in subreddit.stream.comments(skip_existing=True):
     print(comment.body)
     print("  ")
 
+    #! TODO
+    #! 1. Find comment that has 'football_stats_bot (POS, Name, year(optional))'
+    #! 2. Filter out everything in the comment using regex. All we want is want is ( POS, Name, year(optional) )
+    #! 3. Determine if the year parameter was entered, if not then use the current year(2021) by default
+    #! 4. Format the result into JSON so we can query the SQLite3 database with it 
+    if comment.body == "football_stats_bot": 
+        """ Need to respond to 'football_stats_bot (WR, C. Kupp, year(optional))'    """
+        
+        if comment.body has 'WR':
+            pass
+            if comment.body
+        
+        elif comment.body has 'QB':
+            pass
+
+        elif comment.body has 'RB':
+            pass
+
+        elif comment.body has 'K':
+            pass
+
+        elif comment.body has 'TE':
+            pass
+
+        else:
+            print("That is not a position I recognize")
 
 
-"""    
 
 
-for submission in subreddit.top(limit=1):
-    print("************")
-    print('SUBMISSION TITLE : ' + submission.title)
-    print("************")
-    ''' for comment in submission.comments:
+
+
+
+
+
+
+
+"""
         if hasattr(comment, "body"):
             comment_lower = comment.body.lower()
             if " Cooper Kupp " in comment_lower:
                 print("-----------------")
                 print(comment.body)
- '''
-   
-    #! &#x200B; == Blank space (in comment post)
-    #! Gets the comment but not the nested comments
-    # **TO DO ** Figure out how to get nested comments
-    for comment in submission.comments:
-        counter = comment
-        print(' ')
-        print('START OF NEW COMMENT')
-        print('-----------------')
-        print(' ')
-        print('comment # ' + str(comment))
-        print(comment.body)
-        if comment.replies:
-            for reply in comment.replies:
-                print('+++++++++++')
-                print(' ')
-                print('REPLY:', reply.body)
-
-
-
-
-
 """
