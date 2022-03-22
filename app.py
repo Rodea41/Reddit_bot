103b67f10f8531ff8fb476ffdcb190977b3a0d0f
Chris Rodea <Chrisrodea@gmail.com>
2022-03-21 21:40:15 -0700

    Changed app,py to get streaming comments instead of static comments already posted

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import praw
 
+#! Login credentials are in the "praw.ini" file
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("learnpython")
+subreddit = reddit.subreddit("Eldenring")
 ##subreddit = reddit.subreddit("AedorBotTest")
 
-
+#! Prints the name of the account that is logged in
 print(reddit.user.me())
+
+for comment in subreddit.stream.comments(skip_existing=True):
+    print("This is a comment ")
+    print(comment.body)
+    print("  ")
+
+
+
+"""    
 
 
 for submission in subreddit.top(limit=1):
@@ -37,3 +47,9 @@ for submission in subreddit.top(limit=1):
                 print('+++++++++++')
                 print(' ')
                 print('REPLY:', reply.body)
+
+
+
+
+
+"""
